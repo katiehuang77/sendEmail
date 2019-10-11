dade979242261b31567d0a2a436ad72100262d8e
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-11 15:44:35 +0800

    xx

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = {},
     path = require('path')
-conf.HOST                             = '10.172.94.87';
+conf.HOST                             = '127.0.0.1';
 conf.PORT                             =  2222;
 conf.paths                            = {};
 conf.paths.app                        = path.join(__dirname, 'app');
